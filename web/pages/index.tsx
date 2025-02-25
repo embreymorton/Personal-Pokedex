2faab16bb76bce1162b6dd2d94388146cc92e236
embreymorton <111366508+embreymorton@users.noreply.github.com>
2025-02-25 18:45:12 -0500

    Fetch Pokedex

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,34 @@
 /** Home page. */
 
+import { Pokedex } from "@/data/models/pokedex";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 export default function Home() {
-  return <p className="text-lg font-bold">Hello, world!</p>;
+
+  const [page, setPage] = useState<number>(0);
+  const [pokedex, setPokedex] = useState<Pokedex>();
+
+
+  const fetcher = async (): Promise<Pokedex> => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${page*50 + 1}`);
+    const data = await response.json() as unknown as Pokedex;
+    setPokedex(data);
+    return data;
+  }
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['pokedex', page],
+    queryFn: fetcher
+}); 
+
+
+
+return (
+  <div>
+      {isLoading ? <p>Loading...</p> : null}
+      {error ? <p>Error: {error.message}</p> : null}
+      {data ? <p>{data.results.length}</p> : null}
+  </div>
+)
 }
