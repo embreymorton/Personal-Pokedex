0f0d21e7d94941430bcca3989c200ae65d491b20
embreymorton <111366508+embreymorton@users.noreply.github.com>
2025-02-25 20:21:35 -0500

    Clean code

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -8,14 +8,12 @@ import { useState } from "react";
 export default function Home() {
 
   const [page, setPage] = useState<number>(0);
-  const [pokedex, setPokedex] = useState<{ name: string; url: string }[]>();
 
   const maxPage = 27;
 
   const fetcher = async (): Promise<Pokedex> => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${page*50}`);
     const data = await response.json() as unknown as Pokedex;
-    setPokedex(data.results);
     return data;
   }
 
@@ -26,15 +24,16 @@ export default function Home() {
 
 return (
   <div>
-      {isLoading ? <p>Loading...</p> : null}
-      {error ? <p>Error: {error.message}</p> : null}
-
-      <button onClick={() => setPage(page + 1)} disabled={page >= maxPage - 1}>Next Page</button>
-      <button onClick={() => setPage(page - 1)} disabled={page <= 0}>Previous Page</button>
-
-      {pokedex?.map((pokemon) => (
+    <button onClick={() => setPage(page + 1)} disabled={page >= maxPage - 1}>Next Page</button>
+    <button onClick={() => setPage(page - 1)} disabled={page <= 0}>Previous Page</button>
+    
+    {isLoading ? <p>Loading...</p> : null}
+    {error ? <p>Error: {error.message}</p> : null}
+    {data ? (
+      data.results.map((pokemon) => (
         <PokeCard key={pokemon.name} name={pokemon.name} url={pokemon.url} />
-      ))}
+      ))
+    ) : null}
   </div>
 )
 }
