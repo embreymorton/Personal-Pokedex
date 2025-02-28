ea0689a27c7ea0c892a6e60c685f7c8e9be8155b
embreymorton <111366508+embreymorton@users.noreply.github.com>
2025-02-28 09:40:17 -0500

    Learned By Style

--- web/pages/move/[name].tsx
+++ web/pages/move/[name].tsx
@@ -1,3 +1,5 @@
+import PokeCard from "@/components/PokeCard";
+import TypeCard from "@/components/TypeCard";
 import { Move } from "@/data/models/move";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
@@ -9,13 +11,37 @@ export default function MoveDetailsPage({move} : MoveDetailsPageProps) {
     const router = useRouter();
 
     return (
-        <div className="p-6 bg-slate-300 h-screen">
-            <button className="bg-white text-black text-2xl w-24 h-10 border-4 border-slate-700 rounded-xl mb-4" type="button" onClick={() => router.back()}> Back </button>
-            <h1 className="text-4xl">{move.name} details!</h1>
-            <p> Description: {move.flavor_text_entries[0].flavor_text}</p>
-            <p> Type: {move.type.name}</p>
-            <p> Power: {move.power}</p>
-            <p> Accuracy: {move.accuracy}</p>
+        <div className="p-6 bg-slate-300">
+            <button className="bg-white text-black text-2xl min-w-24 h-10 border-4 border-slate-700 rounded-xl mb-4" type="button" onClick={() => router.back()}> Back </button>
+
+            <div className="flex flex-row items-center justify-center gap-4">
+                <div className="p-6 bg-white text-black border-4 border-slate-700 rounded-xl w-80 text-center">
+                    <h1 className="text-6xl mb-4">{move.name}</h1>
+                    <p className="text-2xl"> {move.flavor_text_entries[0].flavor_text}</p>
+                </div>
+
+                <div className="p-6 bg-white text-black border-4 border-slate-700 rounded-xl w-80 text-2xl">
+                    <div className="flex flex-row gap-2 items-center justify-center">
+                        <p> Type: </p>
+                        <TypeCard name={move.type.name} />
+                    </div>
+                    <p className="text-center mt-4"> Power: {move.power || "N/A"}</p>
+                    <p className="text-center mt-4"> Accuracy: {move.accuracy || "N/A"}</p>
+                </div>
+            </div>
+
+            <h2 className="flex text-5xl font-bold items-center justify-center p-4 text-slate-700"> Learned By </h2>
+            <div className="flex flex-wrap gap-8 w-full items-center justify-center">
+                    {move.learned_by_pokemon ? (
+                      move.learned_by_pokemon.map((pokemon) => (
+                        <div key={pokemon.name} className="">
+                          <PokeCard name={pokemon.name}/>
+                        </div>
+            
+                      ))
+                    ) : null}
+                  </div>
+            
         </div>
     )
 }
