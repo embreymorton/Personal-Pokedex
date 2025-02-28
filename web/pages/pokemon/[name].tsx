5c6dcec01339ad3277544e9c7e392af671d9c1e4
embreymorton <111366508+embreymorton@users.noreply.github.com>
2025-02-28 02:48:40 -0500

    Detail Styling

--- web/pages/pokemon/[name].tsx
+++ web/pages/pokemon/[name].tsx
@@ -35,65 +35,65 @@ export default function PokemonDetailsPage({pokemon, species} : PokemonDetailsPa
 
     return (
         <div className="p-4 bg-slate-300">
-            <button className="bg-[#F0F0F0] text-black text-2xl w-20 rounded-xl mb-4" type="button" onClick={() => router.back()}> Back </button>
+            <button className="bg-[#F0F0F0] text-black text-2xl w-24 h-10 border-4 border-slate-700 rounded-xl mb-4" type="button" onClick={() => router.back()}> Back </button>
             
             <div className="flex flex-row items-center justify-center gap-8 ">
-                <div className="bg-[#F0F0F0] flex flex-col items-center justify-center w-[30rem] rounded-3xl border-8 border-slate-700" style={{borderColor: typeColors[pokemon.types[0].type.name].background}}>
+            <div className="bg-[#F0F0F0] flex flex-col items-center justify-center w-[30rem] rounded-3xl border-8 border-slate-700" style={{borderColor: typeColors[pokemon.types[0].type.name].background}}>
         
-                    <div className="text-slate-700 text-4xl mr-auto p-8">
-                        <p>{formatPokedexIndex(pokemon.id)}</p>
-                    </div>
-                    <h1 className="p-6 text-7xl text-slate-700 font-bold"> {formatName(pokemon.name)} </h1>
-
-                    <button onClick={() => setDirection(!direction)}>
-                        {direction ? 
-                            <Image src={pokemon.sprites?.front_default || ''} alt={pokemon.name || 'Pokemon'} width={250} height={250} /> : 
-                            <Image src={pokemon.sprites?.back_default  || ''} alt={pokemon.name || 'Pokemon'} width={250} height={250} />}   
-                    </button> 
+                <div className="text-slate-700 text-4xl mr-auto p-8">
+                <p>{formatPokedexIndex(pokemon.id)}</p>
                 </div>
+                <h1 className="mb-2 text-7xl text-slate-700 font-bold"> {formatName(pokemon.name)} </h1>
 
-                <div className="flex flex-col gap-4 max-w-[25rem]">
-                    <div className="flex bg-[#F0F0F0] text-slate-700 p-4 rounded-xl border-8 border-slate-700">
-                        <p className="text-3xl">{species.flavor_text_entries[0].flavor_text}</p>
-                    </div>
-                    
-                    <div className="flex flex-row gap-4">
-                        <div className="flex flex-col w-[550px] gap-2 bg-[#F0F0F0] text-slate-700 p-4  rounded-xl border-8 border-slate-700 items-center">
-                            <div className="flex flex-row gap-10 mt-4 mb-4">
-                                {pokemon.types.map((type) => (
-                                    <div key={type.slot}>
-                                        <p className="text-3xl ml-1 mb-3 font-bold"> Type {type.slot} </p>
-                                        <TypeCard name={type.type.name}></TypeCard>
-                                    </div>
-                                ))}
-                            </div>
-                            <p className="text-3xl font-bold mb-4">Height: {size.height}</p>
-                            <p className="text-3xl font-bold">Weight: {size.weight}</p>
-                        </div>
-                        
-                    </div>
-                </div>
-                
-                {species.evolves_from_species ? 
-                                <div className="bg-[#F0F0F0] p-4 w-[26rem] h-[30.5rem] border-8 rounded-2xl border-slate-700">
-                                    <div className="flex flex-col items-center">
-                                        <p className="text-2xl mt-2 mb-4 text-slate-700 font-bold">Evolves From</p>
-                                        <PokeCard name={species.evolves_from_species.name} ></PokeCard> 
-                                    </div> 
-                                </div>: null}
-
+                <button className="mb-4" onClick={() => setDirection(!direction)}>
+                {direction ? 
+                    <Image src={pokemon.sprites?.front_default || ''} alt={pokemon.name || 'Pokemon'} width={250} height={250} /> : 
+                    <Image src={pokemon.sprites?.back_default  || ''} alt={pokemon.name || 'Pokemon'} width={250} height={250} />}   
+                </button> 
             </div>
 
-           
+            <div className="flex flex-col gap-4 max-w-[25rem]">
+                <div className="flex bg-[#F0F0F0] text-slate-700 p-4 rounded-xl border-8 border-slate-700">
+                <p className="text-3xl text-center">{species.flavor_text_entries[0].flavor_text}</p>
+                </div>
+                
+                <div className="flex flex-row gap-4">
+                <div className="flex flex-col w-[550px] gap-2 bg-[#F0F0F0] text-slate-700 p-4  rounded-xl border-8 border-slate-700 items-center">
+                    <div className="flex flex-row gap-10 mt-4 mb-4">
+                    {pokemon.types.map((type) => (
+                        <div key={type.slot}>
+                        <p className="text-3xl ml-1 mb-3 font-bold"> Type {type.slot} </p>
+                        <TypeCard name={type.type.name}></TypeCard>
+                        </div>
+                    ))}
+                    </div>
+                    <p className="text-3xl font-bold mb-2">Height: {size.height}</p>
+                    <p className="text-3xl font-bold mb-2">Weight: {size.weight}</p>
+                </div>
+                
+                </div>
+            </div>
+            
+            {species.evolves_from_species ? 
+                    <div className="bg-[#F0F0F0] p-4 w-[26rem] h-[30.5rem] border-8 rounded-2xl border-slate-700">
+                        <div className="flex flex-col items-center">
+                        <p className="text-2xl mt-2 mb-4 text-slate-700 font-bold">Evolves From</p>
+                        <PokeCard name={species.evolves_from_species.name} ></PokeCard> 
+                        </div> 
+                    </div>: null}
 
+            </div>
         
-            <h2 className="text-2xl font-bold"> Moves </h2>
+            <h2 className="flex text-5xl font-bold items-center justify-center p-4 text-slate-700"> Moves </h2>
+            <div className="grid grid-cols-6 gap-4 max-w-[85rem] mx-auto">
             {pokemon.moves.map((move, index) => (
                 <Link href={`/move/${move.move.name}`} key={index}>
-                    <p>{move.move.name}</p>
+                <div className="flex flex-row bg-[#F0F0F0] items-center justify-center rounded-xl p-2 border-slate-700 border-4">
+                    <p className="text-slate-700 text-xl font-bold">{move.move.name}</p>
+                </div>
                 </Link>
             ))}
-            
+            </div>
         </div>
 
     )
