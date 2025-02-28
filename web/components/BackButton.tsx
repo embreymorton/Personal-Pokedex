f0b3304af34150f7f07400ccdf01d07153c1b699
embreymorton <111366508+embreymorton@users.noreply.github.com>
2025-02-28 17:45:24 -0500

    Dark Mode Styling

--- web/components/BackButton.tsx
+++ web/components/BackButton.tsx
@@ -5,7 +5,7 @@ export default function BackButton() {
 
   return (
     <button
-      className="bg-white dark:bg-gray-700 text-slate-700 dark:text-white text-3xl w-48 h-16 border-[6px] border-slate-700 dark:border-white rounded-xl mb-4 ml-32 transition-all duration-300 hover:scale-105"
+      className="bg-white dark:bg-gray-800 text-slate-700 dark:text-white text-3xl w-48 h-16 border-[6px] border-slate-700 dark:border-gray-500 rounded-xl mb-4 ml-32 transition-all duration-300 hover:scale-105"
       type="button"
       onClick={() => router.back()}
     >
